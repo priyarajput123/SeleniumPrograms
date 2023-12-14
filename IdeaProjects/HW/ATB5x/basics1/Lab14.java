2992c795b7cf46d02ca8d8889b42e9dedee92845
priyarajput123 <priya9156rajput@gmail.com>
2023-12-14 16:16:23 +0530

    Java Program

---
+++ IdeaProjects/HW/ATB5x/basics1/Lab14.java
@@ -0,0 +1,15 @@
+package basics1;
+
+public class Lab14 {
+    public static void main(String[] args) {
+        int Enum =9;
+        System.out.println(Enum);
+        //  byte b =128;
+        int a =1, b=2,c=10;
+        boolean bool = true;
+        boolean bool2 =false;
+        // int priya =null; Non-primitive reference type of variable
+        String name = null;
+    }
+
+}
